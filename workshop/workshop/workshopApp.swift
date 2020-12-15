930386387ba9b625b9da1ea2d07c2d39638ea71d
Emma Stålesjö <emma.stalesjo@kits.se>
2020-12-15 11:35:05 +0100

    create hello world project

---
+++ workshop/workshop/workshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  workshopApp.swift
+//  workshop
+//
+//  Created by Emma Stålesjö on 2020-12-15.
+//
+
+import SwiftUI
+
+@main
+struct workshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
